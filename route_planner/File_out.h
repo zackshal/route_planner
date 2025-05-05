32e277eae10c6c56a79e2a63cb9b337cc3080ad7
zackshal <shalash.z2005@gmail.com>
2025-05-06 00:19:50 +0300

    Добавьте файлы проекта.

---
+++ route_planner/File_out.h
@@ -0,0 +1,11 @@
+#ifndef FILE_OUT_H
+#define FILE_OUT_H
+
+#include "Var_str.h"
+
+void print_map_with_path(int** cells,
+    int width,
+    int height,
+    DLinkedList* path);
+
+#endif /* FILE_OUT_H */
